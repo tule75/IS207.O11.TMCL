eb56b940e7ef0a2aa507867dc96fb156808f22e2
Luong Thi Thuy Diem <21521953@gm.uit.edu.vn>
2023-10-18 13:20:50 +0700

    Comlete product-detail page, show less/more text

--- Resources/javascript/category.js
+++ Resources/javascript/category.js
@@ -1,13 +1,16 @@
-const showbtn = document.querySelector('.show');
-const passage = document.querySelector('.texting');
+const showbtn = document.querySelector(".show");
+const textBox = document.querySelector(".text-box");
 
-
-showbtn.addEventListener("click" , () => {
-    if(showbtn.classList.contains('less')) {
-        passage.style.height = "50px";
-        showbtn.innerHTML = "Show more";
-       showbtn.classList.remove("less");
-    } else if(showbtn.classList.contains("more")) {
-       showbtn.classList.add("less");
+showbtn.addEventListener("click", () => {
+    if(showbtn.classList.contains("less")) {
+        showbtn.classList.remove("less");
+        textBox.style.height = "fit-content";
+        showbtn.textContent ="Show less";
+    } else {
+        showbtn.classList.add("less");
+        textBox.style.height = "98px";
+        showbtn.textContent ="Show more";
     }
 })
+
+const img = document.querySelector(".hover-img")
