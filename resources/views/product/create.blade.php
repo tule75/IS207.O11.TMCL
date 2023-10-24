645cd55c7d18edbd9d4834098bbbc21564345076
UIT-21522738 <96582896+UIT-21522738@users.noreply.github.com>
2023-10-24 20:57:21 +0700

    push

---
+++ resources/views/product/create.blade.php
@@ -0,0 +1,13 @@
+<x-guest-layout>
+    <form method="post" action="product.store">
+        Tên
+        <input type="text" name="name">
+        nhập file
+        <input type="file" name="image">
+        price
+        <input type="number" name="price">
+        quantity
+        <input type="number" name="quantity">
+        <button>clickkkkkkkkkkk</button>
+    </form>
+</x-guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\ProfileController;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +22,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get("/test", function () {
+    $result = User::get();
+    dd($result);
+});
+
+Route::get("/product/create", [ProductsController::class, 'create'])->middleware('guest')->name('product.store');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
