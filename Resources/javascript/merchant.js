27d1437ebe76e71526d731f53ef792853ce69521
Luong Thi Thuy Diem <21521953@gm.uit.edu.vn>
2023-10-25 16:57:10 +0700

    Complete merchant site

---
+++ Resources/javascript/merchant.js
@@ -0,0 +1,45 @@
+const bar = document.querySelector('.icon-bars');
+const inputSearch = document.querySelector('.input-box');
+const mainLeft = document.querySelector('.main-left');
+const selectList = document.querySelector('.selection');
+const selects = document.querySelectorAll('.select-click');
+const selectContents = document.querySelectorAll('.select-container');
+
+bar.addEventListener('click',() => {
+    if(inputSearch.classList.contains('hidden')) {
+        inputSearch.classList.remove('hidden');
+        selectList.classList.remove('hidden');
+        mainLeft.style.width = '350px';
+    } else {
+        inputSearch.classList.add('hidden');
+        selectList.classList.add('hidden');
+        mainLeft.style.width = '90px';
+    }
+})
+
+// selects.forEach((item,index) => {
+//     const content = selectContents[index];
+//     item.addEventListener('click',() => {
+//         document.querySelector('select-click.active').classList.remove('active');
+//         document.querySelector('.select-container.active').classList.remove('active');
+//         this.classList.add('active');
+//         content.classList.add('active');
+//     })
+
+// })
+
+selects.forEach((item,index) => {
+    const content = selectContents[index];
+    item.onclick = () => {
+        document.querySelectorAll('.select-click.active').forEach((element) => {
+            element.classList.remove('active');
+        });
+        
+        // Xóa lớp 'active' khỏi tất cả các phần tử .select-container
+        document.querySelectorAll('.select-container.active').forEach((element) => {
+            element.classList.remove('active');
+        });
+        item.classList.add("active");
+        content.classList.add("active");
+    }
+})
