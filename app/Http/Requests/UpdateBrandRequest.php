c244c29ba8cab2128ee01b1bc06c235e5c307120
UIT-21522738 <96582896+UIT-21522738@users.noreply.github.com>
2023-10-27 22:56:14 +0700

    add CRM of Brand Category Payments

---
+++ app/Http/Requests/UpdateBrandRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateBrandRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        if ($this->user()->role == 'manager') {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+            'name' => ['string', 'required']
+        ];
+    }
+}
