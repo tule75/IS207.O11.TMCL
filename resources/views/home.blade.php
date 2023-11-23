67a388b0652eaf5ef6ef7b50b17318dcebd6afeb
UIT.21521875 <21521875@gm.uit.edu.vn>
2023-11-23 22:26:47 +0700

    Tạo giao diện chatbox và thêm chức năng xóa wishlist

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,6 +16,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="{{ asset('/icon/fontawesome-free-6.4.2-web/css/regular.min.css')}}">
     <link rel="stylesheet" href="{{ asset('/css/home.css') }}">
+    <link rel="stylesheet" href="chatbox.css">
 </head>
 
 <body>
@@ -271,7 +272,7 @@
                 </div>
             </div>
         </div>
-
+        
         <div class="pagination-image">
             <img class="pagination-img" src="{{ asset('/img/leather.png')}}" alt="">
         </div>
@@ -279,7 +280,6 @@
         <ul class="pagination">
         </ul>
         <!-- pagination end -->
-
         <!-- main end -->
         <div class="space"></div>
         <!-- extra infomation start -->
@@ -372,6 +372,7 @@
 </body>
 
 <script src="{{ asset('js/home.js')}}"></script>
+
 
 
 
